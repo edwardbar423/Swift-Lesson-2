d29c8d060c60daf879dc2d55ede0d79758c5c5ee
Andrew Barber <edwardbar423@gmail.com>
2016-09-02 14:39:58 -0400

    Swift Lesson 2 file for playground man

---
+++ Swift Lesson 2.playground/Contents.swift
@@ -0,0 +1,158 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+//Number of Dogs
+5
+
+//Number of Cats
+4
+
+//Number of Turtles
+3
+
+//Total number of Animals
+5 + 4 + 3
+
+//Total number of mammals
+5 + 4
+
+// Another person is bringing a dog
+//Number of Dogs
+6
+
+//A turtule is sick.
+//Number of Turtles
+2
+
+//Another person is bringing a Cat
+//Number of Cats
+5
+
+//Another person is bringing a Hamster
+//Number of Hamsters
+1
+
+//Total number of animals (UPDATED)
+6 + 5 + 2 + 1
+
+//Total number of Mammals (UPDATED)
+6 + 5 + 1
+
+let numberOfDogs = 6 + 2
+
+let numberOfCats = 5 - 1
+
+let numberOfTurtles = 2 + 1
+
+let numberOfHamsters = 1 + 1
+
+let numberOfPotBelliedPigs = 1
+
+let numberOfFish = 1
+
+let numberOfSnakes = 1
+
+let totalNumberOfAnimals = numberOfDogs + numberOfCats + numberOfTurtles + numberOfHamsters + numberOfPotBelliedPigs + numberOfSnakes + numberOfFish
+
+let totalNumberOfMammals = numberOfDogs + numberOfCats + numberOfHamsters + numberOfPotBelliedPigs
+
+//--------------------------------------------------------------------------------------------
+
+
+let numberOfTickets = 150
+
+let ticketPrice = 10
+
+let roomRentalFee = 1000
+
+let posterCost = 40
+
+let totalTicketValue = numberOfTickets * ticketPrice
+
+let totalExpenses = roomRentalFee + posterCost
+
+let totalIncomeOfShow = totalTicketValue - totalExpenses
+
+
+//--------------------------------------------------------------------------------------------
+
+
+
+let ticketsSoldLotto = 1050
+let ticketPriceLotto = 2
+let printingCostsLotto = 20
+let advertisingLotto = 50
+
+let totalTakings = ticketPriceLotto * ticketsSoldLotto
+
+let jackpot = totalTakings / 2
+
+let totalExpensesLotto = printingCostsLotto + advertisingLotto
+
+let profit = totalTakings - jackpot - totalExpensesLotto
+
+let programmersCut = profit / 10
+
+let friendsCut = profit - programmersCut
+
+//---------------------------------------------------------------------------------------------
+
+let iPhoneMB = 8000
+let iPhoneMemory = 3000
+let videoMemory = 150
+
+let memoryLeft = iPhoneMB - iPhoneMemory
+let videoSpace = memoryLeft / videoMemory
+
+//You have 33 minutes of video left on your phone.
+//--------------------------------------------------------------------------------------------
+
+let wakeUp = 5
+let brushTeeth = 1
+let breakfast = 15
+let bathe = 8
+let putOnClothing = 2
+
+let morningTime = wakeUp + brushTeeth + breakfast + bathe + putOnClothing
+
+let haveToDo = brushTeeth + putOnClothing
+let likeToDo = wakeUp + breakfast + bathe
+
+//--------------------------------------------------------------------------------------------
+
+let crateOfOranges = 14
+let crateOfWatermelon = 3
+let orangesWeight = 100
+let waterMelonWeight = 200
+let towOranges = crateOfOranges * orangesWeight
+let towWatermelon = crateOfWatermelon * waterMelonWeight
+let totalTowLoad = towOranges + towWatermelon
+let eachSide = totalTowLoad / 2
+let leftHandSideOranges = eachSide / orangesWeight
+let rightHandSideOranges = crateOfOranges - leftHandSideOranges
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
